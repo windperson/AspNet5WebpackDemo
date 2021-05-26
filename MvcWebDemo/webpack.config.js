26c8363feb79ef82fee6c1a7ada32ee77e38e320
Charles Pao <windperson@gmail.com>
2021-05-26 11:55:48 +0800

    add editorconfig & restrict to use Dart Sass

--- MvcWebDemo/webpack.config.js
+++ MvcWebDemo/webpack.config.js
@@ -6,67 +6,73 @@ const bundleFileName = 'bundle';
 const dirName = 'wwwroot/dist';
 
 module.exports = (env, argv) => {
-    return {
-        mode: argv.mode === "production" ? "production" : "development",
-        devtool: argv.mode === "production" ? 'hidden-source-map' : 'source-map',
-        entry: [
-            './node_modules/jquery/dist/jquery.js',
-            './node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js',
-            './node_modules/bootstrap/dist/js/bootstrap.bundle.js',
-            './wwwroot/js/site.js',
-            './wwwroot/scss/site.scss'
-        ],
-        output: {
-            filename: bundleFileName + '.js',
-            path: path.resolve(__dirname, dirName),
-            clean: true
-        },
-        module: {
-            rules: [
-                {
-                    test: require.resolve("jquery"),
-                    loader: "expose-loader",
-                    options: {
-                        exposes: ["$", "jQuery"],
-                    },
-                },
-                {
-                    test: /\.s[c|a]ss$/,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        { loader: 'css-loader', options: { sourceMap: true } },
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                postcssOptions: {
-                                    plugins: function () {
-                                        let plugins = [require('autoprefixer')];
-                                        if (argv.mode === "production") {
-                                            plugins.push(require('cssnano'));
-                                        }
-                                        return plugins;
-                                    }
-                                },
-                                sourceMap: true
-                            }
-                        },
-                        { loader: 'sass-loader', options: { sourceMap: true } }
-                    ]
-                }
-            ]
-        },
-        optimization: {
-            minimize: true,
-            minimizer: [
-                // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-                `...`,
-                new CssMinimizerPlugin(),
-            ],
-        },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: bundleFileName + '.css'
-            })
-        ]
-    }
+	return {
+		mode: argv.mode === "production" ? "production" : "development",
+		devtool: argv.mode === "production" ? 'hidden-source-map' : 'source-map',
+		entry: [
+			'./node_modules/jquery/dist/jquery.js',
+			'./node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js',
+			'./node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+			'./wwwroot/js/site.js',
+			'./wwwroot/scss/site.scss'
+		],
+		output: {
+			filename: bundleFileName + '.js',
+			path: path.resolve(__dirname, dirName),
+			clean: true
+		},
+		module: {
+			rules: [
+				{
+					test: require.resolve("jquery"),
+					loader: "expose-loader",
+					options: {
+						exposes: ["$", "jQuery"],
+					},
+				},
+				{
+					test: /\.s[c|a]ss$/,
+					use: [
+						MiniCssExtractPlugin.loader,
+						{ loader: 'css-loader', options: { sourceMap: true } },
+						{
+							loader: 'postcss-loader',
+							options: {
+								postcssOptions: {
+									plugins: function () {
+										let plugins = [require('autoprefixer')];
+										if (argv.mode === "production") {
+											plugins.push(require('cssnano'));
+										}
+										return plugins;
+									}
+								},
+								sourceMap: true
+							}
+						},
+						{
+							loader: 'sass-loader', options: {
+								sourceMap: true,
+								// Prefer `dart-sass`
+								implementation: require("sass")
+							}
+						},
+					]
+				}
+			]
+		},
+		optimization: {
+			minimize: true,
+			minimizer: [
+				// For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+				`...`,
+				new CssMinimizerPlugin(),
+			],
+		},
+		plugins: [
+			new MiniCssExtractPlugin({
+				filename: bundleFileName + '.css'
+			})
+		]
+	}
 };
